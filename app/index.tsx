24cd8a3695dfdf358c4b0b95dc4e23f7ef0ab30b
jeancdt <flwss157@gmail.com>
2026-03-16 20:58:19 -0300

    feat: filtrar por usuario id

--- app/index.tsx
+++ app/index.tsx
@@ -1,59 +1,62 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import {
-  ActivityIndicator,
-  FlatList,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, TextInput, View } from "react-native";
 
 export default function Index() {
-    const [posts, setPosts] = useState<
-        { id: number; title: string; body: string }[] | null
-    >(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [search, setSearch] = useState(``);
-    const filteredPosts = posts?.filter(p => p.title.includes(search));
+  const [posts, setPosts] = useState<
+    | {
+        userId: number;
+        id: number;
+        title: string;
+        body: string;
+      }[]
+    | null
+  >(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState(``);
+  const [filteredUserId, setFilteredUserId] = useState<number | null>(null);
+  const filteredPosts = posts?.filter(
+    (p) => p.title.includes(search) && (filteredUserId === null || p.userId === filteredUserId),
+  );
 
-    useEffect(() => {
-        const loadPosts = async () => {
-            // const response = await fetch(
-            //     `https://jsonplaceholder.typicode.com/posts`
-            // );
-            // const data = await response.json();
-            const { data } = await axios.get(
-                `https://jsonplaceholder.typicode.com/posts`
-            );
-            setPosts(data);
-            setIsLoading(false);
-        };
+  useEffect(() => {
+    const loadPosts = async () => {
+      const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+      setPosts(data);
+      setIsLoading(false);
+    };
 
     loadPosts();
   }, []);
 
-    if (isLoading) return <ActivityIndicator size="large" />;
+  if (isLoading) return <ActivityIndicator size="large" />;
 
-    return (
-        <View style={{ flex: 1 }}>
-            <Text>Edit app/index.tsx to edit this screen.</Text>
-            <TextInput
-                placeholder="Buscar..."
-                onChangeText={setSearch}
-                value={search}
-            />
-            <FlatList
-                data={filteredPosts}
-                keyExtractor={item => item.id.toString()}
-                renderItem={({ item }) => (
-                    <View style={{ marginBottom: 10 }}>
-                        <Text style={{ fontWeight: `bold` }}>
-                            {item.title.toUpperCase()}
-                        </Text>
-                        <Text>{item.body}</Text>
-                    </View>
-                )}
-            />
-        </View>
-    );
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput placeholder="Buscar..." onChangeText={setSearch} value={search} />
+      <select
+        value={filteredUserId ?? ""}
+        onChange={(e) => setFilteredUserId(e.target.value ? Number.parseInt(e.target.value) : null)}
+      >
+        <option value="">Filtrar por ID de usuário</option>
+
+        {[...new Set(posts?.map((p) => p.userId))].map((userId) => (
+          <option key={userId} value={userId}>
+            ID do usuário: {userId}
+          </option>
+        ))}
+      </select>
+      <FlatList
+        data={filteredPosts}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={{ marginBottom: 10 }}>
+            <Text style={{ fontWeight: `bold` }}>{item.title.toUpperCase()}</Text>
+            <Text>{item.body}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
