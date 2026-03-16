8e798a565805b5903cb74d9dd6e50830302e1768
victor.mazutti <victor.mazuttidev@gmail.com>
2026-03-16 20:23:14 -0300

    feat: adiciona filtragem de itens

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,49 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
-import Aguarde from './Components/Loading';
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
 export default function Index() {
     const [posts, setPosts] = useState<
         { id: number; title: string; body: string }[] | null
     >(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [search, setSearch] = useState(``);
+    const filteredPosts = posts?.filter(p => p.title.includes(search));
 
     useEffect(() => {
         const loadPosts = async () => {
-            const response = await fetch(
+            // const response = await fetch(
+            //     `https://jsonplaceholder.typicode.com/posts`
+            // );
+            // const data = await response.json();
+            const { data } = await axios.get(
                 `https://jsonplaceholder.typicode.com/posts`
             );
-            const data = await response.json();
             setPosts(data);
+            setIsLoading(false);
         };
 
         loadPosts();
     }, []);
 
-    if (!posts) return <Aguarde message="Calma respira" />;
+    if (isLoading) return <ActivityIndicator size="large" />;
 
     return (
         <View style={{ flex: 1 }}>
             <Text>Edit app/index.tsx to edit this screen.</Text>
+            <TextInput
+                placeholder="Buscar..."
+                onChangeText={setSearch}
+                value={search}
+            />
             <FlatList
-                data={posts}
+                data={filteredPosts}
                 keyExtractor={item => item.id.toString()}
                 renderItem={({ item }) => (
                     <View style={{ marginBottom: 10 }}>
